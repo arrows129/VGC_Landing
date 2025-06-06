11293b93fa1295009f29dc9165d9c306a6b47207
arnav8876 <103061707+arrows129@users.noreply.github.com>
2025-06-06 17:12:35 +0530

    Current

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,34 +1,37 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
+
 import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
 export default function CallToAction() {
     const [isHovered, setIsHovered] = useState(false);
-    const animation = useRef<AnimationPlaybackControls>(null);
+    const animation = useRef<AnimationPlaybackControls | null>(null);
     const [scope, animate] = useAnimate();
 
     useEffect(() => {
+        if (!scope.current) return;
+
         animation.current = animate(
             scope.current,
             { x: "-50%" },
-            { duration: 30, ease: "linear", repeat: Infinity }
-        );
-    }, []);
-
-
-    useEffect(()=>{
-        if(animation.current){
-            if(isHovered){
-                animation.current.speed = 1;
-            }else{
-                animation.current.speed = 3;
+            {
+                duration: 30,
+                ease: "linear",
+                repeat: Infinity,
             }
+        );
+    }, [scope, animate]);
+
+    useEffect(() => {
+        if (animation.current) {
+            animation.current.speed = isHovered ? 1 : 3;
         }
-    },[isHovered])
+    }, [isHovered]);
+
     return (
         <section className="py-24">
-            <div className="overflow-x-clip p-4 flex ">
+            <div className="overflow-x-clip p-4 flex">
                 <motion.div
                     ref={scope}
                     className="flex flex-none gap-16 pr-16 text-6xl md:text-7xl font-medium group cursor-pointer"
@@ -40,7 +43,9 @@ export default function CallToAction() {
                             <span className="text-lime-400 text-7xl group-hover:text-white">
                                 &#10038;
                             </span>
-                            <span className="group-hover:text-lime-400">Try a demo</span>
+                            <span className="group-hover:text-lime-400">
+                                Try a demo
+                            </span>
                         </div>
                     ))}
                 </motion.div>
